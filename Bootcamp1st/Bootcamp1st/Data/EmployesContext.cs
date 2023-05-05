cc87032c5f45011df0a0462d20a9de64873e79ac
beatrizrpinheiro <beatrizp.gestaoemp@gmail.com>
2023-05-05 18:32:02 -0300

    adding API folder

---
+++ Bootcamp1st/Bootcamp1st/Data/EmployesContext.cs
@@ -0,0 +1,15 @@
+﻿using Bootcamp1st.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bootcamp1st.Data
+{
+    public class EmployesContext : DbContext
+    {
+        public EmployesContext(DbContextOptions<EmployesContext> options)
+            : base (options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; } = default!;
+    }
+}
